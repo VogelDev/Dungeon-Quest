2747f257dac9b8e3e59f13e2640d480162c0d171
Rob Vogel <robtvogel@gmail.com>
2014-10-24 11:26:29 -0400

    EfficEdit
    Removed unused variables

--- core/src/com/onequest/dungeon/RoomStatue.java
+++ core/src/com/onequest/dungeon/RoomStatue.java
@@ -4,49 +4,48 @@ import java.util.Arrays;
 
 import com.onequest.dungeon.coordinates.Position;
 
-
 public class RoomStatue extends RoomObject {
 
-	public RoomStatue(Position pos, Position worldLoc, Position size, boolean[] isMovable) {
+	public RoomStatue(Position pos, Position worldLoc, Position size,
+			boolean[] isMovable) {
 		super(pos, worldLoc, size, isMovable);
 		this.image = TileImages.getTileImages(TileImages.STATUE);
-		float sizeRatio = image.getWidth() / size.x;
-		this.size.x = size.x;
-		this.size.y = size.x * 2;
-		setBounds();
-	}
-	
-	public RoomStatue(Position pos, Position worldLoc, Position size, boolean isMovable) {
-		super(pos, worldLoc, size, isMovable);
-		this.image = TileImages.getTileImages(TileImages.STATUE);
-		float sizeRatio = image.getWidth() / size.x;
 		this.size.x = size.x;
 		this.size.y = size.x * 2;
 		setBounds();
 	}
 
-	public RoomStatue(Position pos, Position worldLoc, Position size, boolean[] isMovable, int statueType) {
+	public RoomStatue(Position pos, Position worldLoc, Position size,
+			boolean isMovable) {
 		super(pos, worldLoc, size, isMovable);
-		this.image = TileImages.getTileImages(statueType);
-		float sizeRatio = image.getWidth() / size.x;
+		this.image = TileImages.getTileImages(TileImages.STATUE);
 		this.size.x = size.x;
 		this.size.y = size.x * 2;
 		setBounds();
 	}
-	
-	public RoomStatue(Position pos, Position worldLoc, Position size, boolean isMovable, int statueType) {
+
+	public RoomStatue(Position pos, Position worldLoc, Position size,
+			boolean[] isMovable, int statueType) {
 		super(pos, worldLoc, size, isMovable);
 		this.image = TileImages.getTileImages(statueType);
-		float sizeRatio = image.getWidth() / size.x;
 		this.size.x = size.x;
 		this.size.y = size.x * 2;
 		setBounds();
 	}
-	
+
+	public RoomStatue(Position pos, Position worldLoc, Position size,
+			boolean isMovable, int statueType) {
+		super(pos, worldLoc, size, isMovable);
+		this.image = TileImages.getTileImages(statueType);
+		this.size.x = size.x;
+		this.size.y = size.x * 2;
+		setBounds();
+	}
+
 	@Override
-	public void move(Position playerPos){
+	public void move(Position playerPos) {
 		super.move(playerPos);
-		
+
 		Arrays.fill(isMovable, false);
 	}
 
