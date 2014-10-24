1746e2e74619d06f3db4f49d3aeff5526ed862cf
VogelDev <rvogel@mc3.edu>
2014-10-23 22:12:29 -0400

    Version correction
    Updated to proper resolution.
    
    Started version control

--- desktop/src/com/onequest/dungeon/desktop/DesktopLauncher.java
+++ desktop/src/com/onequest/dungeon/desktop/DesktopLauncher.java
@@ -3,10 +3,17 @@ package com.onequest.dungeon.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.onequest.dungeon.MainClass;
-
+/**
+ * Desktop launcher wrapper for project
+ * @author Rob Vogel
+ * @version 0.0.0001
+ *
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new MainClass(), config);
 	}
 }
